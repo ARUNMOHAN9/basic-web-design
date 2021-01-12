6966c7482ca80d692acb6f5339037b0539940e21
Arun Mohan <arunmh7@gmail.com>
2021-01-12 15:22:51 +0530

    bacckground image creation

---
+++ src/app/natours/first-scroll-view/first-scroll-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-scroll-view',
+  templateUrl: './first-scroll-view.component.html',
+  styleUrls: ['./first-scroll-view.component.scss']
+})
+export class FirstScrollViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/natours/natours.module.ts
+++ src/app/natours/natours.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NatoursComponent } from './natours.component';
 import { NatourRoutingModule } from './natour-routing.module';
+import { FirstScrollViewComponent } from './first-scroll-view/first-scroll-view.component';
 
 @NgModule({
-  declarations: [NatoursComponent],
+  declarations: [NatoursComponent, FirstScrollViewComponent],
   imports: [CommonModule, NatourRoutingModule],
 })
 export class NatoursModule {}
