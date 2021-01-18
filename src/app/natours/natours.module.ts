40e73400de5243b9a821f054bb570d38208386d8
Arun Mohan <arunmh7@gmail.com>
2021-01-18 23:41:40 +0530

    added utility classes and minor fixes

--- src/app/natours/natours.module.ts
+++ src/app/natours/natours.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { NatoursComponent } from './natours.component';
 import { NatourRoutingModule } from './natour-routing.module';
 import { FirstScrollViewComponent } from './first-scroll-view/first-scroll-view.component';
+import { SecondScrollViewComponent } from './second-scroll-view/second-scroll-view.component';
 
 @NgModule({
-  declarations: [NatoursComponent, FirstScrollViewComponent],
+  declarations: [NatoursComponent, FirstScrollViewComponent, SecondScrollViewComponent],
   imports: [CommonModule, NatourRoutingModule],
 })
 export class NatoursModule {}
---
+++ src/app/natours/second-scroll-view/second-scroll-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-second-scroll-view',
+  templateUrl: './second-scroll-view.component.html',
+  styleUrls: ['./second-scroll-view.component.scss']
+})
+export class SecondScrollViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
