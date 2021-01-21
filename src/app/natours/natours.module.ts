3cefcc6505b06e02eb38be5d2dad044e504aebb4
Arun Mohan <arunmh7@gmail.com>
2021-01-21 23:38:41 +0530

    folder structure change

--- src/app/natours/natours.module.ts
+++ src/app/natours/natours.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NatoursComponent } from './natours.component';
 import { NatourRoutingModule } from './natour-routing.module';
-import { NatIntroComponent } from './nat-intro/nat-intro.component';
-import { NatAboutComponent } from './nat-about/nat-about.component';
-import { NatFeatureComponent } from './nat-feature/nat-feature.component';
-import { NatToursComponent } from './nat-tours/nat-tours.component';
-import { NatPlainCardComponent } from './nat-plain-card/nat-plain-card.component';
-import { NatAnimateCardComponent } from './nat-animate-card/nat-animate-card.component';
+import { NatIntroComponent } from './components/nat-intro/nat-intro.component';
+import { NatAboutComponent } from './components/nat-about/nat-about.component';
+import { NatFeatureComponent } from './components/nat-feature/nat-feature.component';
+import { NatToursComponent } from './components/nat-tours/nat-tours.component';
+import { NatPlainCardComponent } from './components/nat-plain-card/nat-plain-card.component';
+import { NatAnimateCardComponent } from './components/nat-animate-card/nat-animate-card.component';
 
 @NgModule({
   declarations: [
